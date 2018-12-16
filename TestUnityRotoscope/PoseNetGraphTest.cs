1abb2ac6ccaf08b78da2b0d7646331a1fe1a02be
seadavis <45022599+seadavis@users.noreply.github.com>
2018-12-15 19:15:06 -0700

    added unit tests

---
+++ TestUnityRotoscope/PoseNetGraphTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Assets.Sources.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityEngine;
+
+namespace TestUnityRotoscope
+{
+    [TestClass]
+    public class PoseNetGraphTest
+    {
+        [TestMethod]
+        public void UpdateSingleKeyPoint()
+        {
+            
+
+        }
+
+        [TestMethod]
+        public void UpdateMultipleKeyPoints()
+        {
+            
+        }
+    
+
+        [TestMethod]
+        public void UpdateOneToOne()
+        {
+
+        }
+
+        [TestMethod]
+        public void UpdateManyToOne()
+        {
+
+        }
+
+        /// <summary>
+        /// Sets up a 9 node test graph with enough complexity to test some
+        /// of the more common cases.
+        /// </summary>
+        /// <returns></returns>
+        public PoseNetGraph TestGraph()
+        {
+
+            PoseNetGraph graph = new PoseNetGraph();
+            CharacterGraphNode n1 = new CharacterGraphNode(new NodeTransform(), "n1");
+            CharacterGraphNode n2 = new CharacterGraphNode(
+                                        new NodeTransform(), "n2");
+
+            CharacterGraphNode n3 = new CharacterGraphNode(
+                                        new NodeTransform(), "n3");
+
+            CharacterGraphNode n4 = new CharacterGraphNode(
+                                        new NodeTransform(), "n4");
+
+            CharacterGraphNode n5 = new CharacterGraphNode(
+                                     new NodeTransform(), "n5");
+
+            CharacterGraphNode n6 = new CharacterGraphNode(
+                                     new NodeTransform(), "n6");
+
+            CharacterGraphNode n7 = new CharacterGraphNode(
+                                     new NodeTransform(), "n7");
+
+            CharacterGraphNode n8 = new CharacterGraphNode(
+                                  new NodeTransform(), "n8");
+
+            CharacterGraphNode n9 = new CharacterGraphNode(
+                                 new NodeTransform(), "n9");
+
+
+            graph.AddEdge(new CharacterGraphEdge(n1, n2));
+            graph.AddEdge(new CharacterGraphEdge(n2, n3));
+            graph.AddEdge(new CharacterGraphEdge(n3, n4));
+            graph.AddEdge(new CharacterGraphEdge(n4, n5));
+            graph.AddEdge(new CharacterGraphEdge(n2, n6));
+            graph.AddEdge(new CharacterGraphEdge(n6, n7));
+            graph.AddEdge(new CharacterGraphEdge(n7, n8));
+            graph.AddEdge(new CharacterGraphEdge(n8, n9));
+
+
+            return graph;
+        }
+    }
+}
---
+++ TestUnityRotoscope/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnityRotoscope")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnityRotoscope")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e32860f2-b222-42bb-b127-e5c94e42d7d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
